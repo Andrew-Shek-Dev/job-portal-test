a88a4703a42f268ec31fe4dd2737ae3b5f54137d
Andrew Shek <andrew@tecky.io>
2022-09-06 00:45:05 +0800

    Setup Next.js in Nest.js

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from 'next/app'
-import React from 'react'
+import { AppProps } from 'next/app';
+import React from 'react';
 
-const App = ({Component,pageProps}:AppProps) => {
-  return <Component {...pageProps}/>
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
 
-export default App
+export default App;
