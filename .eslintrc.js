f67647618ce95b8c1236b1942b68c066053766c3
Andrew Shek <andrew@tecky.io>
2022-09-06 16:39:20 +0800

    AWS Amplify Done

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir : __dirname, 
     sourceType: 'module',
+    requireConfigFile: false,
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -25,7 +26,7 @@ module.exports = {
   overrides:[
     {
       "files": ['*.js'],
-      "parser": 'babel-eslint',
+      "parser": '@babel/eslint-parser',
     },
   ]
 };
