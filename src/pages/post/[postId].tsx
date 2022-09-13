eeed097d50a5b2b59f7da977ff1d4523b40f8b78
Andrew Shek <andrew@tecky.io>
2022-09-13 17:52:29 +0800

    [Next JS] ServerProps

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -2,20 +2,27 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const post = () => {
-    const router = useRouter();
-    const {postId} = router.query;
-    const posts = [{id:1},{id:2},{id:3}]
-  return (
-    // <div>Post Id : {postId}</div>
-    <ul>
-        {posts.map(postItem=>(<li key={postItem.id}>
-            <Link href={`/post/${postItem.id}`}>
-                <a>Post Id: {postItem.id}</a>
-                {/*OR {`Post Id: ${postItem.id}`} */}
-            </Link>
-        </li>))}
-    </ul>
-  )
+  //   const router = useRouter();
+  //   const {postId} = router.query;
+  //   const posts = [{id:1},{id:2},{id:3}]
+  // return (
+  //   // <div>Post Id : {postId}</div>
+  //   <ul>
+  //       {posts.map(postItem=>(<li key={postItem.id}>
+  //           <Link href={`/post/${postItem.id}`}>
+  //               <a>Post Id: {postItem.id}</a>
+  //               {/*OR {`Post Id: ${postItem.id}`} */}
+  //           </Link>
+  //       </li>))}
+  //   </ul>
+  // )
+
+  const router = useRouter();
+  const {postId} = router.query;
+
+  if (!postId) return <></>;
+
+  const product = getProductById(postId as string);
 }
 
 export default post;
